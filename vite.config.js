733f32ad9fd4b7413dfa7a7130f0176c0aef2969
HunterLowBit_AWAYFROMHOME <HunterLowBit_AWAYFROMHOME@noreply>
2023-10-20 11:08:30 -0300

    Template

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import tailwindcss from "tailwindcss";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/jogo_simples_js/",
+  plugins: [],
+  resolve: {
+    /*something*/
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./index.html"),
+
+      },
+    },
+  },
+});
