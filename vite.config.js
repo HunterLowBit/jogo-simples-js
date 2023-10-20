754e0b2da27e7c3add2c55963c1161f591e4eaac
HunterLowBit_AWAYFROMHOME <HunterLowBit_AWAYFROMHOME@noreply>
2023-10-20 11:13:57 -0300

    temp

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "./index.html"),
-
+        template: resolve(__dirname, "./src/html/template.html"),
       },
     },
   },
